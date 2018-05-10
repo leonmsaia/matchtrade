6bf96711c3630e291292e94ec00fa45821a4f704
Leonmsaia <leonmsaia@gmail.com>
2018-05-10 19:20:13 -0300

    Fixbug Session Try 3

--- app/config/config.php
+++ app/config/config.php
@@ -11,11 +11,15 @@ if (strpos($url, 'localhost') !== false) {
 if ($appStatus['mode'] == 'production') {
   $config['base_url'] = 'https://matchtrade.herokuapp.com/';
   $config['sess_driver'] = 'database';
-  $config['sess_cookie_name'] = 'ci_session';
   $config['sess_save_path'] = 'ci_sessions';
+  $config['sess_cookie_name']		= 'ci_session';
+  $config['sess_expire_on_close']	= FALSE;
+  $config['sess_encrypt_cookie']	= FALSE;
+  $config['sess_use_database']	= FALSE;
+  $config['sess_table_name']		= 'ci_sessions';
+  $config['sess_match_useragent']	= TRUE;
 }elseif ($appStatus['mode'] == 'development'){
   $config['sess_driver'] = 'files';
-  $config['sess_cookie_name'] = 'ci_session';
   $config['sess_save_path'] = NULL;
   $config['base_url'] = 'http://localhost/matchtrade';
 }
@@ -46,6 +50,7 @@ $config['encryption_key'] = 'jghasdkhasdhgqwotyrefqowduaoskdjhqiwuyrqiwjdpalsjdi
 $config['sess_expiration'] = 7200;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
+$config['sess_cookie_name'] = 'ci_session';
 $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
--- system/libraries/Session/drivers/Session_database_driver.php
+++ system/libraries/Session/drivers/Session_database_driver.php
@@ -6,7 +6,7 @@
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
+ * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,13 +29,14 @@
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
  * @license	http://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
  * @since	Version 3.0.0
  * @filesource
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 /**
  * CodeIgniter Session Database Driver
  *
@@ -46,25 +47,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link	https://codeigniter.com/user_guide/libraries/sessions.html
  */
 class CI_Session_database_driver extends CI_Session_driver implements SessionHandlerInterface {
+
 	/**
 	 * DB object
 	 *
 	 * @var	object
 	 */
 	protected $_db;
+
 	/**
 	 * Row exists flag
 	 *
 	 * @var	bool
 	 */
 	protected $_row_exists = FALSE;
+
 	/**
 	 * Lock "driver" flag
 	 *
 	 * @var	string
 	 */
 	protected $_platform;
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Class constructor
 	 *
@@ -74,9 +80,11 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 	public function __construct(&$params)
 	{
 		parent::__construct($params);
+
 		$CI =& get_instance();
 		isset($CI->db) OR $CI->load->database();
 		$this->_db = $CI->db;
+
 		if ( ! $this->_db instanceof CI_DB_query_builder)
 		{
 			throw new Exception('Query Builder not enabled for the configured database. Aborting.');
@@ -89,6 +97,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			throw new Exception('Configured database connection has cache enabled. Aborting.');
 		}
+
 		$db_driver = $this->_db->dbdriver.(empty($this->_db->subdriver) ? '' : '_'.$this->_db->subdriver);
 		if (strpos($db_driver, 'mysql') !== FALSE)
 		{
@@ -98,13 +107,16 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			$this->_platform = 'postgre';
 		}
+
 		// Note: BC work-around for the old 'sess_table_name' setting, should be removed in the future.
 		if ( ! isset($this->_config['save_path']) && ($this->_config['save_path'] = config_item('sess_table_name')))
 		{
 			log_message('debug', 'Session: "sess_save_path" is empty; using BC fallback to "sess_table_name".');
 		}
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Open
 	 *
@@ -120,9 +132,12 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			return $this->_fail();
 		}
+
 		return $this->_success;
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Read
 	 *
@@ -137,16 +152,20 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			// Prevent previous QB calls from messing with our queries
 			$this->_db->reset_query();
+
 			// Needed by write() to detect session_regenerate_id() calls
 			$this->_session_id = $session_id;
+
 			$this->_db
 				->select('data')
 				->from($this->_config['save_path'])
 				->where('id', $session_id);
+
 			if ($this->_config['match_ip'])
 			{
 				$this->_db->where('ip_address', $_SERVER['REMOTE_ADDR']);
 			}
+
 			if ( ! ($result = $this->_db->get()) OR ($result = $result->row()) === NULL)
 			{
 				// PHP7 will reuse the same SessionHandler object after
@@ -156,20 +175,25 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				$this->_fingerprint = md5('');
 				return '';
 			}
+
 			// PostgreSQL's variant of a BLOB datatype is Bytea, which is a
 			// PITA to work with, so we use base64-encoded data in a TEXT
 			// field instead.
 			$result = ($this->_platform === 'postgre')
 				? base64_decode(rtrim($result->data))
 				: $result->data;
+
 			$this->_fingerprint = md5($result);
 			$this->_row_exists = TRUE;
 			return $result;
 		}
+
 		$this->_fingerprint = md5('');
 		return '';
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Write
 	 *
@@ -183,6 +207,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 	{
 		// Prevent previous QB calls from messing with our queries
 		$this->_db->reset_query();
+
 		// Was the ID regenerated?
 		if (isset($this->_session_id) && $session_id !== $this->_session_id)
 		{
@@ -190,6 +215,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 			{
 				return $this->_fail();
 			}
+
 			$this->_row_exists = FALSE;
 			$this->_session_id = $session_id;
 		}
@@ -197,6 +223,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			return $this->_fail();
 		}
+
 		if ($this->_row_exists === FALSE)
 		{
 			$insert_data = array(
@@ -205,19 +232,23 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				'timestamp' => time(),
 				'data' => ($this->_platform === 'postgre' ? base64_encode($session_data) : $session_data)
 			);
+
 			if ($this->_db->insert($this->_config['save_path'], $insert_data))
 			{
 				$this->_fingerprint = md5($session_data);
 				$this->_row_exists = TRUE;
 				return $this->_success;
 			}
+
 			return $this->_fail();
 		}
+
 		$this->_db->where('id', $session_id);
 		if ($this->_config['match_ip'])
 		{
 			$this->_db->where('ip_address', $_SERVER['REMOTE_ADDR']);
 		}
+
 		$update_data = array('timestamp' => time());
 		if ($this->_fingerprint !== md5($session_data))
 		{
@@ -225,14 +256,18 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				? base64_encode($session_data)
 				: $session_data;
 		}
+
 		if ($this->_db->update($this->_config['save_path'], $update_data))
 		{
 			$this->_fingerprint = md5($session_data);
 			return $this->_success;
 		}
+
 		return $this->_fail();
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Close
 	 *
@@ -246,7 +281,9 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 			? $this->_fail()
 			: $this->_success;
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Destroy
 	 *
@@ -261,24 +298,30 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			// Prevent previous QB calls from messing with our queries
 			$this->_db->reset_query();
+
 			$this->_db->where('id', $session_id);
 			if ($this->_config['match_ip'])
 			{
 				$this->_db->where('ip_address', $_SERVER['REMOTE_ADDR']);
 			}
+
 			if ( ! $this->_db->delete($this->_config['save_path']))
 			{
 				return $this->_fail();
 			}
 		}
+
 		if ($this->close() === $this->_success)
 		{
 			$this->_cookie_destroy();
 			return $this->_success;
 		}
+
 		return $this->_fail();
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Garbage Collector
 	 *
@@ -291,11 +334,14 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 	{
 		// Prevent previous QB calls from messing with our queries
 		$this->_db->reset_query();
+
 		return ($this->_db->delete($this->_config['save_path'], 'timestamp < '.(time() - $maxlifetime)))
 			? $this->_success
 			: $this->_fail();
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Get lock
 	 *
@@ -314,6 +360,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				$this->_lock = $arg;
 				return TRUE;
 			}
+
 			return FALSE;
 		}
 		elseif ($this->_platform === 'postgre')
@@ -324,11 +371,15 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				$this->_lock = $arg;
 				return TRUE;
 			}
+
 			return FALSE;
 		}
+
 		return parent::_get_lock($session_id);
 	}
+
 	// ------------------------------------------------------------------------
+
 	/**
 	 * Release lock
 	 *
@@ -342,6 +393,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 		{
 			return TRUE;
 		}
+
 		if ($this->_platform === 'mysql')
 		{
 			if ($this->_db->query("SELECT RELEASE_LOCK('".$this->_lock."') AS ci_session_lock")->row()->ci_session_lock)
@@ -349,6 +401,7 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				$this->_lock = FALSE;
 				return TRUE;
 			}
+
 			return FALSE;
 		}
 		elseif ($this->_platform === 'postgre')
@@ -358,8 +411,10 @@ class CI_Session_database_driver extends CI_Session_driver implements SessionHan
 				$this->_lock = FALSE;
 				return TRUE;
 			}
+
 			return FALSE;
 		}
+
 		return parent::_release_lock();
 	}
 }
